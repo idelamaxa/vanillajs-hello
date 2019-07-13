cbc48398cffe56cdacbfb78729cf94ae9a8f63a8
idelamaxa <idelamaxa@gmail.com>
2019-07-13 01:16:29 +0000

    generador de excusas

--- src/js/index.js
+++ src/js/index.js
@@ -8,4 +8,21 @@ import "../style/index.scss";
 
 window.onload = function() {
   console.log("Hello Rigo from the console!");
+
+  let who = ["the dog", "my granma", "his turtle", "my bird"];
+  let what = ["eat", "pissed", "crushed", "broked"];
+  let when = [
+    "before the class",
+    "right in time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+  let ran1 = Math.round(Math.random() * who.length);
+  let ran2 = Math.round(Math.random() * what.length);
+  let ran3 = Math.round(Math.random() * when.length);
+
+  let excuse = who[ran1] + " " + what[ran2] + " " + when[ran3];
+  console.log(excuse);
+  document.getElementById("excuse").innerHTML = excuse;
 };
